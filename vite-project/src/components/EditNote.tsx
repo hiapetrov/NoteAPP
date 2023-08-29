b85d137fa8192080681aa34df67a53a65b3f24b2
Alexander <hi.apetrov@gmail.com>
2023-08-29 17:07:47 +0100

    cleared-errors-onSubmit

--- vite-project/src/components/EditNote.tsx
+++ vite-project/src/components/EditNote.tsx
@@ -1,18 +1,21 @@
 import { NoteData , Tag} from "../App"
 import NoteForm from "./NoteForm"
+import { useNote } from "./NoteLayout"
 
 export type EditNoteProps = {
-    onSubmit : (data: NoteData) => void
+    onSubmit : (id: string, data: NoteData) => void
     onAddTag: (tag: Tag) => void
     availableTags: Tag[]
 }
 
 export default function EditNote( { onSubmit, onAddTag, availableTags }: EditNoteProps) {
+    const note = useNote()
+
     return (
         <>
         <h1 className="mb-4">Edit Note</h1>
         <NoteForm 
-            onSubmit={onSubmit}
+            onSubmit={data => onSubmit(note.id, data)}
             onAddTag={onAddTag}
             availableTags={availableTags}/>
         </>
